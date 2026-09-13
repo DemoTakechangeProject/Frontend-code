a61763f18284d592d61f20f60f2522dacc7cb5c6
pritish-Development <wadhavanepritish@gmail.com>
2026-09-13 17:53:43 +0530

    13-09 session

---
+++ Javascript/ArrowFun.js
@@ -0,0 +1,37 @@
+// function add(a,b){
+//     return a+b;
+// }
+// let op = add(10,20);
+
+// console.log("Addition :"+op);
+
+// Arrow Function
+
+// const add = (a,b) => {
+//     console.log(a+b);  
+// }
+
+// const add = (a,b) =>a+b;
+
+// const add = (a,b) =>{
+//     let ans = a+b;
+//     console.log(ans);
+// };
+
+// add(30,30);
+
+
+// const evenOdd = number =>{
+
+// }
+
+const evenOdd =(number)=>{
+    if(number%2==0){
+        console.log("Number is even");
+    }else{
+        console.log("Number is Odd");
+    }
+}
+
+
+evenOdd(11);
--- Javascript/variable.js
+++ Javascript/variable.js
@@ -15,95 +15,95 @@
 
 
 // let and const are block-scoped and function-scoped,while var is function-scoped
-{
-  var a=10;
-  // let b=20; // local 
-  // const c=30;
+// {
+//   var a=10;
+//   // let b=20; // local 
+//   // const c=30;
 
-  // console.log(a);
-  // console.log(b);
-  // console.log(c);
-}
-console.log(a);
-// console.log(b);
-// console.log(c);
-
-// function scoped :
-function display(){
-  var a=10;
-  // let b=20; // local 
-  // const c=30;
-
-  // console.log(a);
-  // console.log(b);
-  // console.log(c);
-}
-console.log(a);
-// console.log(b);
-// console.log(c);
-display();
-
-// const must initilize
-const name; // Missing initializer in const declaration
-// name = "Ankita";
-console.log(name);
-
-// check datatype using typeof
-// let roll = 101;
-// let name = "Pritish";
-// let per = 50.40;
-// let isPass = true;
-
-// let num = [10,20,30,40];
-
-// let student = {
-//   name:"Ankita"
+//   // console.log(a);
+//   // console.log(b);
+//   // console.log(c);
 // }
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
 
-// console.log(typeof roll);
-// console.log(typeof name);
-// console.log(typeof per);
-// console.log(typeof isPass);
+// // function scoped :
+// function display(){
+//   var a=10;
+//   // let b=20; // local 
+//   // const c=30;
 
-// console.log(typeof num);
-
-// console.log(typeof student);
-
-// Operator in JS 
-
-// let a=10;
-// let b=20;
-
-// // Arithmetic Op
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-
-// //Comparision opertor
-// console.log(a==b);
-// console.log(a!=b);
-// console.log(a>b);
-// console.log(a<b);
-// console.log(a>=b);
-// console.log(a<=b);
-
-// console.log(a==="10"); // checks both value and data type
-
-// // Logical op
-// console.log(a>=b && a!=b);
-// console.log(a<=b || a!=b);
-
-// funtion declaration and pass value
-// function checkEven(num){
-//   if(num%2 == 0){
-//   console.log("Number is Even!");
-// }else{
-//   console.log("Number is Odd!");
+//   // console.log(a);
+//   // console.log(b);
+//   // console.log(c);
 // }
-// }
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+// display();
 
-// checkEven(11);
+// // const must initilize
+// const name; // Missing initializer in const declaration
+// // name = "Ankita";
+// console.log(name);
+
+// // check datatype using typeof
+// // let roll = 101;
+// // let name = "Pritish";
+// // let per = 50.40;
+// // let isPass = true;
+
+// // let num = [10,20,30,40];
+
+// // let student = {
+// //   name:"Ankita"
+// // }
+
+// // console.log(typeof roll);
+// // console.log(typeof name);
+// // console.log(typeof per);
+// // console.log(typeof isPass);
+
+// // console.log(typeof num);
+
+// // console.log(typeof student);
+
+// // Operator in JS 
+
+// // let a=10;
+// // let b=20;
+
+// // // Arithmetic Op
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+
+// // //Comparision opertor
+// // console.log(a==b);
+// // console.log(a!=b);
+// // console.log(a>b);
+// // console.log(a<b);
+// // console.log(a>=b);
+// // console.log(a<=b);
+
+// // console.log(a==="10"); // checks both value and data type
+
+// // // Logical op
+// // console.log(a>=b && a!=b);
+// // console.log(a<=b || a!=b);
+
+// // funtion declaration and pass value
+// // function checkEven(num){
+// //   if(num%2 == 0){
+// //   console.log("Number is Even!");
+// // }else{
+// //   console.log("Number is Odd!");
+// // }
+// // }
+
+// // checkEven(11);
 
 function display(roll,name,isPass){
   console.log(roll);
